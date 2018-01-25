32cba93776fd95ccbc59b382117e46484928df32
serpient <littlegangwolf@gmail.com>
2018-01-24 23:26:35 -0800

    starting of body and header of main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
-import './App.css';
-
-const logo = require('./logo.svg');
+import './styles/App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="header">
+          <div className="logo">FRANCESCA & XIN</div>
+          <div>SCHEDULE</div>
+          <div>BALI</div>
+          <div>FAQS</div>
+          <div>RSVP</div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className="main-body-text">
+          <div className="landing-text">CELEBRATE WITH US</div>
+          <div className="landing-box">
+            <div className="landing-subtext">in BALI, JUNE 20th 2019</div>
+          </div>
+        </div>
       </div>
     );
   }
